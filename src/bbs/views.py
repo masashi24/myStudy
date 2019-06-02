7961a474e9489b3f269dcdfb7f36a9b3d56f62f1
渡辺将司 <watanabemasashi@watanabemasashinoMacBook-Pro.local>
2019-06-03 03:54:05 +0900

    chapter 1

--- src/bbs/views.py
+++ src/bbs/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Article
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world')
+    articles = Article.objects.all()
+    context = {
+        'message': 'Welcome my BBS',
+        'articles': articles,
+        }
+    return render(request, 'bbs/index.html', context)
 
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message': 'Show Article ' + str(id),
+        'article': article,
+    }
+    return render(request, 'bbs/detail.html', context)
