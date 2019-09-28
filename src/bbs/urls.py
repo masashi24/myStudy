1bf52ab5c4dc710e97b61a329619853fa2db8719
masashi.watanabe@mujin.co.jp <mujin@debian.mujin.co.jp>
2019-09-28 19:46:54 +0900

    aa

--- src/bbs/urls.py
+++ src/bbs/urls.py
@@ -6,6 +6,8 @@ app_name = 'bbs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.detail, name='detail'),
+    path('create', views.create, name='create'),
+    path('<int:id>/delete', views.delete,name='delete'),
 
     path('scraping', views.scraping, name='scraping')
 ]
--- src/bbs/views.py
+++ src/bbs/views.py
@@ -18,8 +18,37 @@ def detail(request, id):
     context = {
         'message': 'Show Article ' + str(id),
         'article': article,
-    }
+        }
     return render(request, 'bbs/detail.html', context)
+
+def create(request):
+    article = Article(content='Hello BBS', user_name='paiza')
+    article.save()
+    
+    articles = Article.objects.all()
+    context = {
+        'message': 'Create article',
+        'articles': articles,
+        }
+    return render(request, 'bbs/index.html', context)
+
+def delete(request, id):
+    article = get_object_or_404(Article, pk=id)
+    article.delete()
+
+    '''articles = Article.objects.all()
+    context = {
+        'message': 'Delete Article ' + str(id),
+        'article': articles,
+        }
+    return render(request, 'bbs/detail.html', context)'''
+
+    articles = Article.objects.all()
+    context = {
+        'message': 'Deleted article' + str(id),
+        'articles': articles,
+        }
+    return render(request, 'bbs/index.html', context)
 
 def scraping(request):
     #articles = Article.objects.all()
