02452c4e1f2d8e596c84e20a17e69131177268ae
masashi.watanabe@mujin.co.jp <mujin@debian.mujin.co.jp>
2019-11-11 08:19:01 +0900

    base

--- src/bbs/models.py
+++ src/bbs/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     content = models.CharField(max_length=200)
     user_name = models.CharField(max_length=200, null=True)
+    title = models.CharField(max_length=200, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.content
--- src/bbs/views.py
+++ src/bbs/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
 from .myRequests import sample
+from .forms import SearchForm
+from .forms import ArticleForm
 
 # Create your views here.
 
 def index(request):
-    articles = Article.objects.all()
+    searchForm = SearchForm(request.GET)
+    if searchForm.is_valid():
+        keyword = searchForm.cleaned_data['keyword']
+        articles = Article.objects.filter(content__contains=keyword)
+    else:
+        searchForm = SearchForm()
+        articles = Article.objects.all()
+
     context = {
         'message': 'Welcome my BBS',
         'articles': articles,
+        'searchForm': searchForm,
         }
     return render(request, 'bbs/index.html', context)
 
@@ -21,27 +31,54 @@ def detail(request, id):
         }
     return render(request, 'bbs/detail.html', context)
 
-def create(request):
-    article = Article(content='Hello BBS', user_name='paiza')
-    article.save()
-    
-    articles = Article.objects.all()
+def new(request):
+    articleForm = ArticleForm()
+
     context = {
-        'message': 'Create article',
-        'articles': articles,
+        'message': 'New Article',
+        'articleForm': articleForm,
+    }
+
+    return render(request, 'bbs/new.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        articleForm = ArticleForm(request.POST)
+        if articleForm.is_valid():
+            article = articleForm.save()
+    
+    context = {
+        'message': 'Create article' + str(article.id),
+        'article': article
         }
-    return render(request, 'bbs/index.html', context)
+    return render(request, 'bbs/detail.html', context)
+
+def edit(request, id):
+    article = get_object_or_404(Article, pk=id)
+    articleForm = ArticleForm(instance=article)
+
+    context = {
+        'message': 'Edit Article',
+        'article': article,
+        'articleForm': articleForm,
+    }
+    return render(request, 'bbs/edit.html', context)
+
+def update(request, id):
+    if request.method == 'POST':
+        article = get_object_or_404(Article, pk=id)
+        articleForm = ArticleForm(request.POST, instance=article)
+        if articleForm.is_valid():
+            articleForm.save()
+    context = {
+        'message': 'Update article ' + str(id),
+        'article': article,
+    }
+    return render(request, 'bbs/detail.html', context)
 
 def delete(request, id):
     article = get_object_or_404(Article, pk=id)
     article.delete()
-
-    '''articles = Article.objects.all()
-    context = {
-        'message': 'Delete Article ' + str(id),
-        'article': articles,
-        }
-    return render(request, 'bbs/detail.html', context)'''
 
     articles = Article.objects.all()
     context = {
